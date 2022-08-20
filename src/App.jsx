fed4625b492fc6a2f9287aa7e6a5f73c2e10c2d4
alejandro111555 <al.mirskoy@gmail.com>
2022-08-20 15:50:05 +0300

    added props to the component Profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { FriendList } from "./components/FriendList/FriendList";
 import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
 import { Container } from "components/ui/Container.styled";
 import data from './data/data.json';
-import friends from './data/friends.json'
-import transactions from './data/transactions.json'
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+import user from './data/user.json';
 import { Box } from "./components/ui/Box";
 
 
@@ -13,7 +14,13 @@ import { Box } from "./components/ui/Box";
 export const App = () => {
   return (
     <Container>
-      <Profile/>
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
       <Box display ="flex">
         <Statistics
           title="Upload stats"
